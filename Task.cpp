67c2c0b30decd932841d40f70177a81b3798d6a4
axz5388 <axz5388@35ad4fb5-8762-4f94-a9b0-6691fac8a32c>
2011-12-14 19:02:10 +0000

    Factored out Task.h
    git-svn-id: svn+ssh://linus.se.rit.edu/home/teams/463/w463-01u1g/svnroot@8 35ad4fb5-8762-4f94-a9b0-6691fac8a32c

--- Task.cpp
+++ Task.cpp
@@ -1,120 +1,4 @@
 
-#include <signal.h>
-#include <sys/siginfo.h>
-#include <time.h>
-
-
-#include <pthread.h>
-#include <semaphore.h>
-
-/**
-* Represents a task to be scheduled by the Scheduler class.
-*/
-class Task {
-  private:
-
-    /**
-    * How many nanoseconds represent one time unit.
-    * This should not exceed 550 milliseconds.
-    */
-    const long UNIT_NANOSECONDS = 1000;
-
-    /**
-    * The number of units that the task should take to run.
-    * The c value in the (c, p, d) triple.
-    */
-    int time;
-
-    /**
-    * The period of the task. Assumed to also be the deadline.
-    */
-    int period;
-
-    /**
-    * The remaining amount of units that this task must run.
-    */
-    int remaining;
-
-    /**
-    * The number id of the task. Added to _NTO_TRACE_USERFIRST
-    * Must be between 0 and (_NTO_TRACE_USERLAST - _NTO_TRACE_USERFIRST)
-    */
-    int taskId;
-
-    /**
-    * Whether this task is critical.
-    * A critical task must finish before the enxt time it is run.
-    * A non-critical task is killed if it misses it's deadline.
-    */
-    bool critical;
-
-    /**
-    * The Scheduler that is scheduling the current task set.
-    */
-    Scheduler * scheduler;
-
-    /**
-    * The current task's thread id.
-    */
-    pthread_t threadId;
-
-    /**
-    * Whether the current task should continue running.
-    */
-    bool runThread;
-
-    /**
-    * The semephore used to block tasks that have completed
-    * but have not yet reached their period (idle tasks).
-    */
-    static sem_t runSemId;
-
-
-  public:
-    Task(Scheduler s, int c, int p, int taskId);
-    ~Task();
-
-    /**
-    *
-    */
-    void setCritical(bool critical);
-
-    /**
-    *
-    */
-    void setPriority(int priority);
-
-    /**
-    *
-    */
-    int getRemaining() {
-        return this->remaining;
-    }
-
-    /**
-    *
-    */
-    void schedule(bool reschedule);
-
-    /**
-     * Registers a timer to call PeriodElapsed every $period microseconds
-     */
-    void Task::RegisterTimer();
-
-  private:
-    /**
-    *
-    */
-    static void *run(void *object);
-
-    /**
-    *
-    */
-    static void PeriodElapsed();
-
-
-}
-
 #include <pthread.h>
 #include <semaphore.h>
 
@@ -164,13 +48,15 @@ static void *Task::run(void *object){
     }
 }
 
-Task::setPriority(int priority) {
-    pthread_setschedparam(...)
+void Task::setPriority(int priority) {
+	pthread_setschedprio(this->taskId, priority);
 }
-Task::setCritical(bool critical) {
+
+void Task::setCritical(bool critical) {
     this->critical = critical;
 }
-Task::schedule(bool reschedule) {
+
+void Task::schedule(bool reschedule) {
     TraceEvent( _NTO_TRACE_INSERTUSRSTREVENT,
                 _NTO_TRACE_USERFIRST + this->taskId,
                 this->remaining == 0 ? "period:1" : "period:0" );
