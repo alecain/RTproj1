7c4a5fdd58aa09cc1f0476eb1b7648af28958220
axz5388 <axz5388@35ad4fb5-8762-4f94-a9b0-6691fac8a32c>
2012-01-09 19:26:45 +0000

    Cleanup.
    git-svn-id: svn+ssh://linus.se.rit.edu/home/teams/463/w463-01u1g/svnroot@27 35ad4fb5-8762-4f94-a9b0-6691fac8a32c

--- RateMonotonicScheduler.cpp
+++ RateMonotonicScheduler.cpp
@@ -7,45 +7,14 @@
 
 #include "RateMonotonicScheduler.h"
 
-RateMonotonicScheduler::RateMonotonicScheduler() {
-	Init(/*&Start*/);
-
-}
-
-RateMonotonicScheduler::~RateMonotonicScheduler() {
-}
-
-//TODO Possibly not needed -- will be defined in base class
-/*void RateMonotonicScheduler::Init() {
-	sem_init(semaphore, 0, 0);
-}*/
-/*
-void RateMonotonicScheduler::Start() {
-	// Starts scheduling thread
-	// iterate through all tasks and start each one
-	vector<Task*>::iterator it;
-
-	for(it=m_tasks.begin(); it < m_tasks.end(); it++) {
-
-	}
-	sem_post(m_pSemaphore);
-}*/
-
 void RateMonotonicScheduler::ScheduleAll() {
-	//sem_wait(m_pSemaphore);
 	vector<Task*>::iterator it;
 	Task* leastPeriodTask;
 
 	for (it = m_tasks.begin(); it < m_tasks.end(); it++) {
-		if((*it)->getPeriod() < leastPeriodTask->getPeriod())
-		{
+		if((*it)->getPeriod() < leastPeriodTask->getPeriod()) {
 			leastPeriodTask = *it;
 		}
 	}
-	leastPeriodTask->setPriority(99); //TODO what value do we enter for the priority here?
-	//it->schedule();
+	leastPeriodTask->setPriority(99);
 }
-
-/*void RateMonotonicScheduler::Reschedule() {
-
-}*/
